474d4cbc1c49e4ad8435f39145c18a846828932c
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-11-25 19:26:30 +0600

    dwrair add doair add

--- src/Components/Layouts/DashboardLayout.js
+++ src/Components/Layouts/DashboardLayout.js
@@ -6,13 +6,28 @@ import Navbar from "../Pages/Share/Navbar/Navbar";
 const DashboardLayout = () => {
   return (
     <div>
-      <div className="bg-gray-800 sticky top-0 left-0 z-[99]">
-        {/* <Navbar /> */}
+      <div className="drawer drawer-end drawer-mobile">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          <label
+            htmlFor="my-drawer-4"
+            className="drawer-button btn btn-primary"
+          >
+            Open drawer
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-base-300 text-base-content">
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="max-w-[1400px] mx-auto">
-        <Outlet />
-      </div>
-      <div className="bg-base-200">{/* <Footer /> */}</div>
     </div>
   );
 };
