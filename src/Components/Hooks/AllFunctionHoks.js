2b516d14005842e0adb5b1ed90531bea3128b8ee
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-11-27 02:15:49 +0600

    add products data post server function add

---
+++ src/Components/Hooks/AllFunctionHoks.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+//image fill host function
+export const HandelImgHost = (imageFile = "") => {
+  const [imgHostLink, setImgHostLink] = useState("");
+
+  const imageHostKey = process.env.REACT_APP_IMGBB_API_KEY;
+  const formData = new FormData();
+  const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`;
+  useEffect(() => {
+    if (!imageFile) {
+      return;
+    }
+    const image = imageFile[0];
+    formData.append("image", image);
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.success) {
+          setImgHostLink(data);
+        }
+      });
+  }, [imageFile]);
+  return [imgHostLink];
+};
--- src/Components/Pages/Dashboard/AddProduct/AddProduct.jsx
+++ src/Components/Pages/Dashboard/AddProduct/AddProduct.jsx
@@ -5,12 +5,16 @@ import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { AuthContext } from "../../../Context/AuthContextProvaider/AuthContextProvaider";
+import { HandelImgHost } from "../../../Hooks/AllFunctionHoks";
 import { serverUrl } from "../../../Hooks/AllUrl/AllUrl";
 import Loading from "../../Share/Loading/Loading";
 import SectionTitle from "../../Share/SectionTitle/SectionTitle";
 
 const AddProduct = () => {
   const [categoryNameData, setCategoryNameData] = useState([]);
+  const { user } = useContext(AuthContext);
+  const [imageFile, setImageFile] = useState("");
+  const [imgHostLink] = HandelImgHost(imageFile);
   const {
     register,
     handleSubmit,
@@ -18,13 +22,13 @@ const AddProduct = () => {
   } = useForm();
 
   const defaultMonthAndDate = new Date().toLocaleString();
-  const { user } = useContext(AuthContext);
-  // const time = format(selected, 'PP')
   const newDate = defaultMonthAndDate.split(",")[0].split("/").join("-");
   const newTiem = new Date().toLocaleTimeString(navigator.language, {
     hour: "2-digit",
     minute: "2-digit",
   });
+
+  // all category name server data load function
   useEffect(() => {
     fetch(`${serverUrl}/all-products-category/`)
       .then((res) => res.json())
@@ -32,7 +36,6 @@ const AddProduct = () => {
   }, []);
 
   // add product function
-  // const { displayName, email } = user;
   const handelAddProduct = (data) => {
     // console.log(data);
     const productData = {
@@ -41,8 +44,10 @@ const AddProduct = () => {
       name: user?.displayName,
       postDate: newDate,
       postTime: newTiem,
+      productImageUrl: imgHostLink?.data?.display_url,
+      sellersVerify: false,
     };
-    console.log(productData);
+    // console.log(productData);
     fetch(`${serverUrl}/add-product`, {
       method: "POST",
       headers: {
@@ -68,7 +73,61 @@ const AddProduct = () => {
         <SectionTitle title={"Add Your Product"} />
       </div>
       <div className="w-3/4 mx-auto">
-        <form className="mt-20" onSubmit={handleSubmit(handelAddProduct)}>
+        <form className="" onSubmit={handleSubmit(handelAddProduct)}>
+          {imgHostLink ? (
+            <div className="flex items-center justify-center mx-auto md:w-3/4 lg:w-2/4  my-10">
+              <div className="w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                <img
+                  className="h-[100%] w-full"
+                  src={imgHostLink?.data?.display_url}
+                  alt="upload img preview"
+                ></img>
+              </div>
+            </div>
+          ) : (
+            <div className="flex items-center justify-center w-full my-10">
+              <label
+                htmlFor="dropzone-file"
+                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    aria-hidden="true"
+                    className="w-10 h-10 mb-3 text-gray-400"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                    ></path>
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">
+                      Click to upload your product images
+                    </span>
+                    or drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    PNG or JPG (MAX. 800x400px)
+                  </p>
+                </div>
+
+                <input
+                  onChange={(e) => setImageFile(e.target.files)}
+                  id="dropzone-file"
+                  type="file"
+                  accept="productImages/jpg, productImages/png"
+                  className="hidden"
+                />
+              </label>
+            </div>
+          )}
+
           <div className="grid md:grid-cols-2 md:gap-6">
             <div className="relative z-0 mb-6 w-full group">
               <input
@@ -238,7 +297,6 @@ const AddProduct = () => {
               </label>
             </div>
           </div>
-
           <div className="relative z-0 mb-6 w-full group ">
             <input
               type="text"
